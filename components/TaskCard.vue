125631d4e4428f57894363a5e286f1a40309d9fc
Mohamad Salimi <smartcontentor@gmail.com>
2021-07-21 13:04:59 +0430

    change card style in tasks page

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -1,15 +1,15 @@
 <template>
   <v-card
     outlined
-    class="pa-3 my-1 d-flex justify-space-between item"
+    class="pa-3 my-1 d-flex align-center justify-space-between item"
+    :class="hoverColor"
     elevation="0"
-    style="cursor:move;"
   >
     <span>
       <v-icon small>mdi-drag-horizontal-variant</v-icon>
       <span>{{ title }}</span>
     </span>
-    <v-btn icon small>
+    <v-btn small text class="btn">
       <v-icon small>mdi-pencil</v-icon>
     </v-btn>
   </v-card>
@@ -17,8 +17,38 @@
 
 <script>
 export default {
-  props: ["title"]
+  props: ["title"],
+  computed: {
+    hoverColor() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.item {
+  z-index: 9999;
+  cursor: pointer;
+  height: 40px;
+}
+
+.btn {
+  display: none;
+  min-width: 0 !important;
+  width: 32px !important;
+  height: 24px;
+}
+
+.item:hover .btn {
+  display: inline-block !important;
+}
+
+.dark:hover {
+  background-color: #424242;
+}
+
+.light:hover {
+  background-color: #f5f5f5;
+}
+</style>
--- components/TaskListCard.vue
+++ components/TaskListCard.vue
@@ -1,14 +1,37 @@
 <template>
   <v-card class="pa-2 ma-3" style="width:300px;height:fit-content;">
-    <v-card-title class="p-0 d-flex justify-space-between">
-      <h4>{{ title }}</h4>
-      <v-icon style="cursor:move;">mdi-drag-horizontal-variant</v-icon>
+    <v-card-title class="px-1 d-flex justify-space-between">
+      <v-text-field
+        v-if="textfield"
+        v-model="title"
+        @blur="showTextField()"
+        @keyup.enter="showTextField()"
+        placeholder="نام لیست"
+        class="ma-0 pa-0"
+        hide-details="auto"
+        outlined
+        dense
+        autofocus
+      ></v-text-field>
+      <h4 style="cursor:pointer;" v-else @click="showTextField()">
+        {{ title }}
+      </h4>
+
+      <v-icon style="cursor:move;" v-if="!textfield"
+        >mdi-drag-horizontal-variant</v-icon
+      >
     </v-card-title>
     <v-btn block color="green darken-1 white--text no-letter-spacing mb-2">
       <v-icon>mdi-plus</v-icon>
       افزودن وظیفه
     </v-btn>
-    <draggable class="list-group" :list="list1" group="items" @change="log">
+    <draggable
+      class="list-group"
+      :class="scrollbarTheme"
+      :list="list1"
+      group="items"
+      @change="log"
+    >
       <TaskCard
         v-for="task in tasks"
         :id="task.id"
@@ -21,12 +44,61 @@
 
 <script>
 export default {
-  props: ["id", "title", "tasks"]
+  props: ["id", "title", "tasks"],
+  computed: {
+    scrollbarTheme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    }
+  },
+  data() {
+    return {
+      textfield: false,
+      defaultTitle: this.title
+    };
+  },
+  methods: {
+    showTextField() {
+      if (this.title) {
+        this.textfield = !this.textfield;
+        this.defaultTitle = this.title;
+      } else {
+        this.textfield = !this.textfield;
+        this.title = this.defaultTitle;
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
 .no-letter-spacing {
   letter-spacing: 0 !important;
+}
+
+.list-group {
+  height: fit-content;
+  max-height: 370px;
+  overflow-x: hidden;
+  overflow: auto;
+  padding: 0 2px;
+}
+
+.list-group::-webkit-scrollbar {
+  width: 15px;
+}
+
+.list-group::-webkit-scrollbar-track {
+  background: #202020;
+  border-left: 1px solid #2c2c2c;
+}
+
+.list-group::-webkit-scrollbar-thumb {
+  background: #3e3e3e;
+  border: solid 3px #202020;
+  border-radius: 7px;
+}
+
+.list-group::-webkit-scrollbar-thumb:hover {
+  background: white;
 }
 </style>
